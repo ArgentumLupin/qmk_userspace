4bd3a0a68e3c0a9b79741c1bb386fa8df242d5e6
Axel Ljungblom <axelljungblom@outlook.com>
2026-09-05 12:51:45 +0000

    Dev/fix : Changed gaming layout and added del func
    Changed the gamming layout to have more keys.
    
    Added that shift escape now acts as delete.
    
    added more junkfiles for the git ignore.

--- keyboards/lily58/keymaps/laxen/keymap.c
+++ keyboards/lily58/keymaps/laxen/keymap.c
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
   [_MID] = LAYOUT(
-  _______,TG(_SEC),TG(_GAME), _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+  _______,TG(_SEC),TG(_GAME), _______, _______, _______,                  _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -185,10 +185,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
   [_GAME] = LAYOUT(
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_Y,    XXXXXXX, KC_I,    KC_O,    KC_P,    XXXXXXX,
-  XXXXXXX, XXXXXXX, KC_A,    KC_S,    KC_D,    XXXXXXX,                   KC_H,    KC_J,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, KC_W,    XXXXXXX, XXXXXXX,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX,
+  XXXXXXX, XXXXXXX, KC_A,    KC_S,    KC_D,    XXXXXXX,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                             XXXXXXX, KC_ESC,  KC_ENT,  KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX, MO(_MID)
+                             XXXXXXX, KC_ESC,  KC_ENT,  KC_SPC,  KC_SPC,  KC_TAB,  XXXXXXX, MO(_MID)
   )
   
 
@@ -257,6 +257,50 @@ bool oled_task_user(void) {
 }
 #endif // OLED_ENABLE
 
+// Initialize variable holding the binary
+// representation of active modifiers.
+uint8_t mod_state;
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    // Store the current modifier state in the variable for later reference
+    mod_state = get_mods();
+    switch (keycode) {
+
+    case KC_BSPC:
+        {
+        // Initialize a boolean variable that keeps track
+        // of the delete key status: registered or not?
+        static bool delkey_registered;
+        if (record->event.pressed) {
+            // Detect the activation of either shift keys
+            if (mod_state & MOD_MASK_SHIFT) {
+                // First temporarily canceling both shifts so that
+                // shift isn't applied to the KC_DEL keycode
+                del_mods(MOD_MASK_SHIFT);
+                register_code(KC_DEL);
+                // Update the boolean variable to reflect the status of KC_DEL
+                delkey_registered = true;
+                // Reapplying modifier state so that the held shift key(s)
+                // still work even after having tapped the Backspace/Delete key.
+                set_mods(mod_state);
+                return false;
+            }
+        } else { // on release of KC_BSPC
+            // In case KC_DEL is still being sent even after the release of KC_BSPC
+            if (delkey_registered) {
+                unregister_code(KC_DEL);
+                delkey_registered = false;
+                return false;
+            }
+        }
+        // Let QMK process the KC_BSPC keycode as usual outside of shift
+        return true;
+    }
+
+    }
+    return true;
+};
+
+/*
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
 #ifdef OLED_ENABLE
@@ -266,3 +310,5 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 }
+*/
+
