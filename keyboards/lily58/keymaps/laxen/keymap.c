4172795d9f130c17ec11f4f2ce206d10234c2794
Laxen <axelju@student.chalemers.se>
2026-09-06 13:43:47 +0200

    Fix : switched all ralts to lalts
    you can only switch tty's with left alt so I switched the right alts I
    hade on the right half of the keyboard to send left alts so that it now
    will work poprerly.

--- keyboards/lily58/keymaps/laxen/keymap.c
+++ keyboards/lily58/keymaps/laxen/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-             MO(_RAISE), KC_LALT, KC_LCTL,  LT(_LEFT, KC_SPC), LT(_RIGHT, KC_ENT), KC_BSPC, KC_RALT, MO(_LOWER)
+             MO(_RAISE), KC_LALT, KC_LCTL,  LT(_LEFT, KC_SPC), LT(_RIGHT, KC_ENT), KC_BSPC, KC_LALT, MO(_LOWER)
 ),
 /* RIGHT
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_RIGHT] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______,                   KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_QUOT, KC_LBRC,
-  _______, _______, _______, _______, _______, _______,                   _______, KC_RCTL, KC_RGUI, KC_RALT, KC_RSFT, _______,
+  _______, _______, _______, _______, _______, _______,                   _______, KC_RCTL, KC_RGUI, KC_LALT, KC_RSFT, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                      _______, _______, _______,LT(_MID, KC_SPC), _______,  _______, _______, _______
   ),
